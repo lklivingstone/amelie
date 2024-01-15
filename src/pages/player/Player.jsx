8b80d1a98c4aa519a58477fefb1a8486d686df6d
L K Livingstone <livingstone.lk.337@gmail.com>
2024-01-15 12:16:32 +0530

    Added new player

--- src/pages/player/Player.jsx
+++ src/pages/player/Player.jsx
@@ -1,10 +1,13 @@
-import { Navbar } from "../../shared/components/navbar/Navbar";
 import { useParams } from 'react-router-dom';
+import { useState } from "react";
+
+import { Navbar } from "../../shared/components/navbar/Navbar";
 
 import "./Player.css";
 
 export const Player = () => {
     const { type, id } = useParams();
+    const [toggle, setToggle] = useState(false);  
 
     return (
         <>
@@ -21,6 +24,24 @@ export const Player = () => {
                     ></iframe>
                 </div>
             </div>
+            <button style={{padding: "20px", margin: "2.5vw"}} onClick={(e) => setToggle(!toggle)}>{toggle ? 'true' : 'false'}</button>
+            {
+              toggle && (
+                
+            <div className="container">
+                <div className="player">
+                    <iframe
+                    width="100%"
+                    height="100%"
+                    src={`https://vidsrc.xyz/embed/${type}/${id}`}
+                    title="title"
+                    frameBorder="0"
+                    allowFullScreen
+                    ></iframe>
+                </div>
+            </div>
+              )
+            }
         </>
     )
 }
